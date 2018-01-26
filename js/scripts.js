d3b47021f47141fa353968b62a19ceb8ae185f37
lala <magic@magic.com>
2018-01-25 19:47:20 -0800

    end of day3

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+var sentence = "dfjnsdjfnkjs";
+console.log(sentence);
+{//Block A
+  {//Block B
+    {//Block C
+      {//Block D
+
+      }
+    }
+  }
+}
+
+{let firstNumber = 22; //Declaration and definition
+  console.log(firstNumber);
+}
+var secondNumber = 11;
+// or
+var firstNumber; //declaration
+  firstNumber = 22; //definition
+
+firstNumber = "hello government"; // this redefines the variable (woohoo)
+  console.log(firstNumber);
+
+const truth = "lala is still the shiiiiiiiiiiiit";
+console.log(truth);
+
+const bestMovie = 'resvior dog';
+
+if (typeof(bestMovie) ==='string'){
+  alert('yay its a strong!');
+} else {
+  alert('it\'s not a string or a number!');
+}
+
+switch(typeof(bestMovie)) {
+  case 'string':
+  alert('yay it\'s a string!');
+  break;
+  case 'number':
+  alert('A number!');
+  break;
+  case 'object':
+  alert('Object!');
+  break;
+default:
+alert('its an array');
+break;
+}
+const
