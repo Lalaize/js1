8a8eeb7c3453e6b3f67d5678f8cd2e593184a73d
lala <magic@magic.com>
2018-01-25 18:08:06 -0800

    end of day 2

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var sentence = "dfjnsdjfnkjs";
+console.log(sentence);
+{//Block A
+  {//Block B
+    {//Block C
+      {//Block D
+
+      }
+    }
+  }
+}
+
+{let firstNumber = 22; //Declaration and definition
+  console.log(firstNumber);
+}
+var secondNumber = 11;
+// or
+var firstNumber; //declaration
+  firstNumber = 22; //definition
+
+firstNumber = "hello government"; // this redefines the variable (woohoo)
+  console.log(firstNumber);
+
+const truth = "lala is still the shiiiiiiiiiiiit";
+console.log(truth);
